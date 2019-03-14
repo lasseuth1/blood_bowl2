769838fee8492ba9be9afd653e8854690a7f95b1
lasseuth1 <48207671+lasseuth1@users.noreply.github.com>
2019-03-14 10:46:43 +0100

    Update memory.py

--- rarity_of_events/memory.py
+++ rarity_of_events/memory.py
@@ -11,7 +11,6 @@ class Memory(object):
         action_shape = 1
         self.actions = torch.zeros(num_steps, action_shape)
         self.actions = self.actions.long()
-        #self.masks = torch.ones(num_steps + 1, 1)
 
     def cuda(self):
         self.observations = self.observations.cuda()
@@ -19,7 +18,6 @@ class Memory(object):
         self.value_predictions = self.value_predictions.cuda()
         self.returns = self.returns.cuda()
         self.actions = self.actions.cuda()
-       # self.masks = self.masks.cuda()
 
     def insert(self, step, spatial_obs, non_spatial_obs, action, value_pred, reward):
         self.spatial_obs[step + 1].copy_(spatial_obs)
@@ -27,19 +25,10 @@ class Memory(object):
         self.actions[step].copy_(action)
         self.value_predictions[step].copy_(value_pred)
         self.rewards[step].copy_(reward)
-      #  self.masks[step].copy_(mask)
 
-    def compute_returns(self, next_value, use_gae, gamma, tau=0.1):
-        if use_gae:
-            self.value_predictions[-1] = next_value
-            gae = 0
-            for step in reversed(range(self.rewards.size(0))):
-                delta = self.rewards[step] + gamma * self.value_predictions[step + 1] * self.masks[step] - self.value_predictions[step]
-                gae = delta + gamma * tau * self.masks[step] * gae
-                self.returns[step] = gae + self.value_predictions[step]
-        else:
-            self.returns[-1] = next_value
-            for step in reversed(range(self.rewards.size(0))):
-                self.returns[step] = self.returns[step + 1] * \
-                    gamma + self.rewards[step]
-                    # gamma * self.masks[step] + self.rewards[step]
+    def compute_returns(self, next_value, gamma):
+        self.returns[-1] = next_value
+        for step in reversed(range(self.rewards.size(0))):
+            self.returns[step] = self.returns[step + 1] * \
+                                 gamma + self.rewards[step]
+            # gamma * self.masks[step] + self.rewards[step]
