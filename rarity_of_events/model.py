76d802f5ce62c5fcec13e0c9eb30bd51af015da3
lasseuth1 <48207671+lasseuth1@users.noreply.github.com>
2019-03-14 10:46:21 +0100

    Update model.py

--- rarity_of_events/model.py
+++ rarity_of_events/model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from rarity_of_events.distributions import Categorical
 
 
 class FFPolicy(nn.Module):
@@ -11,18 +10,11 @@ class FFPolicy(nn.Module):
     def forward(self, x, y):
         raise NotImplementedError
 
-    def act(self, spatial_inputs, non_spatial_input, deterministic=False):
+    def act(self, spatial_inputs, non_spatial_input):
         value, policy = self(spatial_inputs, non_spatial_input)
-        # action = self.dist.sample(policy, deterministic=deterministic)
-        #action = policy.max(1)
         probs = F.softmax(policy)
         action = probs.multinomial(1)
         return value, action
-
-    # def evaluate_actions(self, spatial_inputs, non_spatial_input, actions):
-    #     value, x = self(spatial_inputs, non_spatial_input)
-    #     action_log_probs, dist_entropy = self.dist.logprobs_and_entropy(x, actions)
-    #     return value, action_log_probs, dist_entropy
 
     def evaluate_actions(self, spatial_inputs, non_spatial_input):
         value, x = self(spatial_inputs, non_spatial_input)
@@ -32,19 +24,18 @@ class FFPolicy(nn.Module):
 class CNNPolicy(FFPolicy):
     def __init__(self, num_inputs, action_space_shape):
         super(CNNPolicy, self).__init__()
+
         # num_inputs is 26 (number of feature layers)
-        # kernel-size is the size of the filter that runs over the layers
-        self.conv1 = nn.Conv2d(num_inputs, out_channels=52, kernel_size=2)
+        self.conv1 = nn.Conv2d(num_inputs, out_channels=52, kernel_size=3)
         self.conv2 = nn.Conv2d(in_channels=52, out_channels=26, kernel_size=2)
 
+        # Linear layers
         self.linear1 = nn.Linear(49, 98)
         self.linear2 = nn.Linear(98, 49)
 
+        # The actor and the critic outputs
         self.critic = nn.Linear(26 * 5 * 12 + 49, 1)
         self.actor = nn.Linear(26 * 5 * 12 + 49, action_space_shape)
-
-        # num_outputs = 1
-        # self.dist = Categorical(26 * 5 * 12 + 7, num_outputs)
 
         self.train()
         self.reset_parameters()
@@ -69,7 +60,7 @@ class CNNPolicy(FFPolicy):
         # Non-spatial input through two linear layers (fully-connected)
         y = self.linear1(non_spatial_input)
         y = F.relu(y)
-        y = self.linear2(y)
+        y = self.linear2(y) # kan være det ikke er nødvendigt
         y = F.relu(y)
 
         # Concatenate the outputs
@@ -78,12 +69,6 @@ class CNNPolicy(FFPolicy):
 
         # Return value, policy
         return self.critic(concatenated), self.actor(concatenated)
-
-    def get_probs(self, inputs):
-        value, x = self(inputs)
-        x = self.dist(x)
-        probs = F.softmax(x)
-        return probs
 
     def get_action_probs(self, spatial_input, non_spatial_input):
         x, y = self(spatial_input, non_spatial_input)
