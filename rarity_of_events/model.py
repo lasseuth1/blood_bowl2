81461f6bbeedd56bdd56209e17bbe4d0749b2228
lasseuth1 <48207671+lasseuth1@users.noreply.github.com>
2019-03-19 11:22:41 +0100

    Update model.py

--- rarity_of_events/model.py
+++ rarity_of_events/model.py
@@ -10,14 +10,37 @@ class FFPolicy(nn.Module):
     def forward(self, x, y):
         raise NotImplementedError
 
-    def act(self, spatial_inputs, non_spatial_input):
-        value, policy = self(spatial_inputs, non_spatial_input)
-        probs = F.softmax(policy)
-        action = probs.multinomial(1)
-        return value, action
+    def act(self, spatial_inputs, non_spatial_input, avail_actions, env):
+        value, policy, position = self(spatial_inputs, non_spatial_input)
+
+        probs_action = F.softmax(policy)
+        probs_action = probs_action[0]
+
+        mask = torch.ones(len(probs_action), dtype=torch.uint8)
+        for action in avail_actions:
+            mask[action] = 0
+        probs_action [mask] = 0
+        action = probs_action.multinomial(1)
+
+        probs_position = F.softmax(position)
+        # probs_position = probs_position[0].view(7, 14)
+        probs_position = probs_position[0]
+
+        avail_positions = env.available_positions(action)
+        if len(avail_positions) != 0:
+            mask = torch.ones(7 * 14, dtype=torch.uint8)
+            for pos in avail_positions:
+                new_pos = pos.x + 14 * pos.y
+                mask[new_pos] = 0
+            probs_position[mask] = 0
+            position = probs_position.multinomial(1)
+        else:
+            position = []
+
+        return value, action, position
 
     def evaluate_actions(self, spatial_inputs, non_spatial_input):
-        value, x = self(spatial_inputs, non_spatial_input)
+        value, x, _ = self(spatial_inputs, non_spatial_input)
         return F.softmax(x), value
 
 
@@ -26,16 +49,18 @@ class CNNPolicy(FFPolicy):
         super(CNNPolicy, self).__init__()
 
         # num_inputs is 26 (number of feature layers)
-        self.conv1 = nn.Conv2d(num_inputs, out_channels=52, kernel_size=3)
+        self.conv1 = nn.Conv2d(num_inputs, out_channels=52, kernel_size=2)
         self.conv2 = nn.Conv2d(in_channels=52, out_channels=26, kernel_size=2)
 
         # Linear layers
-        self.linear1 = nn.Linear(49, 98)
-        self.linear2 = nn.Linear(98, 49)
+        self.linear1 = nn.Linear(49, 24)
+        # self.linear2 = nn.Linear(98, 49)
 
         # The actor and the critic outputs
-        self.critic = nn.Linear(26 * 5 * 12 + 49, 1)
-        self.actor = nn.Linear(26 * 5 * 12 + 49, action_space_shape)
+        self.critic = nn.Linear(26 * 5 * 12 + 24, 1)
+        self.actor = nn.Linear(26 * 5 * 12 + 24, action_space_shape)
+        # chose a position among the 7 * 14 squares on the board
+        self.position = nn.Linear(26 * 5 * 12 + 24, 7 * 14)
 
         self.train()
         self.reset_parameters()
@@ -45,7 +70,7 @@ class CNNPolicy(FFPolicy):
         self.conv1.weight.data.mul_(relu_gain)
         self.conv2.weight.data.mul_(relu_gain)
         self.linear1.weight.data.mul_(relu_gain)
-        self.linear2.weight.data.mul_(relu_gain)
+        # self.linear2.weight.data.mul_(relu_gain)
 
     def forward(self, spatial_input, non_spatial_input):
         """
@@ -60,15 +85,16 @@ class CNNPolicy(FFPolicy):
         # Non-spatial input through two linear layers (fully-connected)
         y = self.linear1(non_spatial_input)
         y = F.relu(y)
-        y = self.linear2(y) # kan være det ikke er nødvendigt
-        y = F.relu(y)
+        # y = self.linear2(y) # kan være det ikke er nødvendigt
+        # y = F.relu(y)
 
         # Concatenate the outputs
         concatenated = torch.cat((x.flatten(), y.flatten()), dim=0)
-        concatenated = concatenated.view(-1, 26 * 5 * 12 + 49)
+        # Reshaping the tensor to have a dimensions (1, 1609)
+        concatenated = concatenated.view(-1, 26 * 5 * 12 + 24)
 
-        # Return value, policy
-        return self.critic(concatenated), self.actor(concatenated)
+        # Return value, policy, spatial-position
+        return self.critic(concatenated), self.actor(concatenated), self.position(concatenated)
 
     def get_action_probs(self, spatial_input, non_spatial_input):
         x, y = self(spatial_input, non_spatial_input)
