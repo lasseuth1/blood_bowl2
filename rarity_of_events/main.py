643b898f88d2d99bcc214a7d20b00896a7e25b92
lasseuth1 <48207671+lasseuth1@users.noreply.github.com>
2019-07-08 10:01:28 +0200

    Update main.py

--- rarity_of_events/main.py
+++ rarity_of_events/main.py
@@ -6,32 +6,34 @@ import torch.nn as nn
 import numpy as np
 from torch.autograd import Variable
 from rarity_of_events.model import CNNPolicy
+from rarity_of_events.pruned_hybrid import PrunedHybrid
 import torch.optim as optim
 from rarity_of_events.memory import Memory
 import torch.nn.functional as F
 from rarity_of_events.event_buffer import EventBuffer
 from rarity_of_events.vec_env import VecEnv
 import rarity_of_events.arguments as args
+from ffai.ai.renderer import Renderer
+import rarity_of_events.utils as utils
+import time
+import pickle
 
 
 def main():
-    # env = gym.make("FFAI-3-v1")
-
-    best_final_rewards = -1000000.0
-
-    es = [make_env(i, board_size=args.board_size) for i in range(args.num_processes)]
+    es = [make_env(i, args.board_size) for i in range(args.num_processes)]
     envs = VecEnv([es[i] for i in range(args.num_processes)])
 
     spatial_obs_space = es[0].observation_space.spaces['board'].shape
-    non_spatial_space = (1, 49)
+    non_spatial_space = (1, 50)
     action_space = len(es[0].actions)
 
     # MODELS #
-    ac_agent = CNNPolicy(spatial_obs_space[0], action_space)  # New model
-    # ac_agent = torch.load("models/" + args.model_to_load)         # Load model
+    if args.resume:
+        ac_agent = torch.load("models/" + args.model_name)   # Load model
+    else:
+        ac_agent = PrunedHybrid(spatial_obs_space[0], action_space)
 
     optimizer = optim.RMSprop(ac_agent.parameters(), args.learning_rate)
-    # optimizer = optim.Adam(ac_agent.parameters(), learning_rate)
 
     # Creating the memory to store the steps taken
     memory = Memory(args.num_steps, args.num_processes, spatial_obs_space, non_spatial_space)
@@ -42,153 +44,133 @@ def main():
     memory.spatial_obs[0].copy_(torch.from_numpy(spatial_obs).float())
     memory.non_spatial_obs[0].copy_(torch.from_numpy(non_spatial_obs).float())
 
-    # These variables are used to compute average rewards for all processes.
-    episode_rewards = torch.zeros([args.num_processes, 1])
-    final_rewards = torch.zeros([args.num_processes, 1])
-    episode_intrinsic_rewards = torch.zeros([args.num_processes, 1])
-    final_intrinsic_rewards = torch.zeros([args.num_processes, 1])
-    episode_events = torch.zeros([args.num_processes, args.num_events])  # [4,10]
-    final_events = torch.zeros([args.num_processes, args.num_events])
 
-    # Create event-buffer
-    event_buffer = EventBuffer(args.num_events, capacity=args.eb_capacity)
+    if args.resume & args.log:
+        log_file = "logs/" + args.log_filename
+        with open(log_file) as log:
+            lines = log.readlines()[-1]
+            resume_episodes = float(lines.split(", ")[1])
+            resume_updates = float(lines.split(", ")[0])
+            resume_steps = float(lines.split(", ")[2])
+    else:
+        resume_episodes = 0
+        resume_updates = 0
+        resume_steps = 0
 
-    event_episode_rewards = []
-
-    steps = 0
-    dones = 0
-    number_of_touchdowns = 0
-    log = True
-    last_rewards = 0
+    renderer = Renderer()
 
     for update in range(args.num_updates):
 
         for step in range(args.num_steps):
-            steps += 1
+
             available_actions = envs.actions()
-            value, action, action_objects, positions = ac_agent.act(
-                                                         Variable(memory.spatial_obs[step]),
-                                                         Variable(memory.non_spatial_obs[step]),
-                                                         available_actions,
-                                                         envs)
+            active_players = envs.active_players()
+            own_players = envs.own_players()
 
-            obs, reward, done, info, events = envs.step(action_objects)
+            values, actions_policy = ac_agent.act(
+                Variable(memory.spatial_obs[step]),
+                Variable(memory.non_spatial_obs[step]), available_actions)
 
-            intrinsic_reward = []
+            actions, x_positions, y_positions = utils.map_actions_3v3_new_approach(actions_policy, active_players,
+                                                                                   own_players)
+            action_objects = []
 
-            for e in events:
-                if args.rarity_of_events:
-                    intrinsic_reward.append(event_buffer.intrinsic_reward(e))
-                else:
-                    r = reward[len(intrinsic_reward)]
-                    intrinsic_reward.append(r)
+            for action, position_x, position_y in zip(actions, x_positions, y_positions):
 
-            # Format variables correctly
+                action_object = {
+                    'action-type': action,
+                    'x': position_x,
+                    'y': position_y
+                    }
+                action_objects.append(action_object)
+
+            obs, reward, done, info = envs.step(action_objects)
+
+            if args.render:
+                for i in range(args.num_processes):
+                    renderer.render(obs[i], i)
+
             reward = torch.from_numpy(np.expand_dims(np.stack(reward), 1)).float()
-            intrinsic_reward = torch.from_numpy(np.expand_dims(np.stack(intrinsic_reward), 1)).float()
-            events = torch.from_numpy(np.expand_dims(np.stack(events), args.num_events)).float().squeeze(2)
-            # events = torch.from_numpy(events).float()
-            episode_rewards += reward
-            episode_intrinsic_rewards += intrinsic_reward
-            episode_events += events
-
-            # Event stats: Displaying rewards acquired for each event in a list
-            event_rewards = []
-            for ei in range(0, args.num_events):
-                ev = np.zeros(args.num_events)
-                ev[ei] = 1
-                er = event_buffer.intrinsic_reward(ev)
-                event_rewards.append(er)
-
-            event_episode_rewards.append(event_rewards)
 
             # If done then clean the history of observations.
-            if torch.sum(reward) > 0:
-                print()
             masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done])
-            final_rewards *= masks
-            final_intrinsic_rewards *= masks
-            final_events *= masks
-            final_rewards += (1 - masks) * episode_rewards
-            final_intrinsic_rewards += (1 - masks) * episode_intrinsic_rewards
-            final_events += (1 - masks) * episode_events
-
-            for i in range(args.num_processes):
-                if done[i]:
-                    event_buffer.record_events(np.copy(final_events[i].numpy()))
-                    dones += 1
-                    log = False
-
-            episode_rewards *= masks
-            episode_intrinsic_rewards *= masks
-            episode_events *= masks
 
             # Update the observations returned by the environment
             spatial_obs, non_spatial_obs = update_obs(obs)
 
             # insert the step taken into memory
             memory.insert(step, torch.from_numpy(spatial_obs).float(), torch.from_numpy(non_spatial_obs).float(),
-                          action.data, positions.data, value.data, reward, masks)
-
-        final_episode_reward = np.mean(event_episode_rewards, axis=0)
-        event_episode_rewards = []
+                          torch.tensor(actions_policy), torch.tensor(values), reward, masks, available_actions)
 
         next_value = ac_agent(Variable(memory.spatial_obs[-1]), Variable(memory.non_spatial_obs[-1]))[0].data
+
+        # Compute returns
         memory.compute_returns(next_value, args.gamma)
 
-        spatial = Variable(memory.spatial_obs[:-1])             # [20,  4, 26,  7, 14]
-        spatial = spatial.view(-1, *spatial_obs_space)          # [80, 26,  7, 14]
-        non_spatial = Variable(memory.non_spatial_obs[:-1])     # [20,  4,  1, 49]
-        non_spatial = non_spatial.view(-1, 49)                  # [80, 49]
+        spatial = Variable(memory.spatial_obs[:-1])  # shape [20,  4, 26,  7, 14]
+        spatial = spatial.view(-1, *spatial_obs_space)  # shape [80, 26,  7, 14]
+        non_spatial = Variable(memory.non_spatial_obs[:-1])  # shape [20,  4,  1, 49]
+        non_spatial = non_spatial.view(-1, 50)  # shape [80, 49]
 
         actions = Variable(torch.LongTensor(memory.actions.view(-1, 1)))
-        positions = Variable(torch.LongTensor(memory.positions.view(-1, 1)))
+        actions_mask = Variable(memory.available_actions[:-1])
 
-        # [80, 37]   [80, 1]  [80, 99]
-        action_log_probs, values, position_log_probs, dist_entropy = ac_agent.evaluate_actions(Variable(spatial),
-                                                                                               Variable(non_spatial),
-                                                                                               actions, positions)
+        # Evaluate the actions taken
+        action_log_probs, values, dist_entropy = ac_agent.evaluate_actions(Variable(spatial),
+                                                                           Variable(non_spatial),
+                                                                           actions, actions_mask)
 
         values = values.view(args.num_steps, args.num_processes, 1)
         action_log_probs = action_log_probs.view(args.num_steps, args.num_processes, 1)
-        position_log_probs = position_log_probs.view(args.num_steps, args.num_processes, 1)
+
         advantages = Variable(memory.returns[:-1]) - values
         value_loss = advantages.pow(2).mean()
 
+        # Compute loss
         action_loss = -(Variable(advantages.data) * action_log_probs).mean()
-        position_loss = -(Variable(advantages.data) * position_log_probs).mean()
 
         optimizer.zero_grad()
 
-        total_loss = (value_loss + action_loss + position_loss - dist_entropy * 0.0001)
+        total_loss = (value_loss * args.value_loss_coef + action_loss - dist_entropy * args.entropy_coef)
         total_loss.backward()
 
-        nn.utils.clip_grad_norm(ac_agent.parameters(), 0.5)
+        nn.utils.clip_grad_norm_(ac_agent.parameters(), args.max_grad_norm)
 
         optimizer.step()
 
         memory.non_spatial_obs[0].copy_(memory.non_spatial_obs[-1])
         memory.spatial_obs[0].copy_(memory.spatial_obs[-1])
 
-        final_intrinsic_rewards_mean = final_intrinsic_rewards.mean()
-        final_rewards_mean = final_rewards.mean()
-
-        #if (update + 1) % log_interval == 0:
-        if dones % 4 == 0 and log is False:
+        # Logging
+        if (update + 1) % args.log_interval == 0 and args.log:
             log_file_name = "logs/" + args.log_filename
-            total_num_steps = (update + 1) * args.num_processes * args.num_steps
-            # update = update + args.updates_when_stop
-            # total_num_steps = args.timesteps_when_stop + total_num_steps
-            log = "Updates {}, num timesteps {}, mean reward {:.5f}, number of touchdowns {}" \
-                .format(update, total_num_steps, final_rewards_mean, number_of_touchdowns)
-            log_to_file = "{}, {}, {:.5f}, {}\n" \
-                .format(update, total_num_steps, final_rewards_mean, number_of_touchdowns)
+
+            episodes, steps, rewards, wins, touchdowns = envs.log()
+            updates = resume_updates + update + 1
+            this_episodes = episodes.sum()
+            resume_episodes += this_episodes
+            steps = updates * args.num_processes * args.num_steps
+            reward = rewards.sum()
+            wins = wins.sum()
+            touchdowns = touchdowns.sum()
+
+
+            log = "Updates {}, Episodes {}, Episodes this update {}, Timesteps {}, Reward {}, Wins {}, " \
+                  "Touchdowns {}" \
+                .format(updates, resume_episodes, this_episodes, steps, reward, wins, touchdowns
+                        )
+
+            log_to_file = "{}, {}, {}, {}, {}, {}, \n" \
+                .format(updates, resume_episodes, this_episodes, steps, reward, wins, touchdowns
+                        )
+
             print(log)
 
+            # Save to files
             with open(log_file_name, "a") as myfile:
                 myfile.write(log_to_file)
 
-            log = True
+            # Saving the agent
             torch.save(ac_agent, "models/" + args.model_name)
 
 
@@ -249,6 +231,7 @@ def update_obs(observations):
                                      obs['state']['opp kods'],
                                      obs['state']['opp casualties'],
                                      obs['state']['own score'],
+                                     obs['state']['own turns'],
                                      obs['state']['own starting rerolls'],
                                      obs['state']['own rerolls left'],
                                      obs['state']['own ass coaches'],
